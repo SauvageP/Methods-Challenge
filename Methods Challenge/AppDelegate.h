e6f076da4cc0aaa4fd404be9395ac4a539885804
yung305 <prgabriel88@gmail.com>
2014-10-20 18:31:47 -0400

    Initial Commit

---
+++ Methods Challenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Methods Challenge
+//
+//  Created by Perry R Gabriel on 10/20/14.
+//  Copyright (c) 2014 Re/Up. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Methods Challenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Methods Challenge
+//
+//  Created by Perry R Gabriel on 10/20/14.
+//  Copyright (c) 2014 Re/Up. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
